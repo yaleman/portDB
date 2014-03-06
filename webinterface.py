498cf216e4d5f0b9ee1f29381bc20ce9af070a88
James Hodgkinson <yaleman@ricetek.net>
2014-03-06 21:27:06 +1000

    ripped out some testing code

--- webinterface.py
+++ webinterface.py
@@ -1,22 +1,15 @@
 #!/usr/bin/python
 
 
-#from flask import Flask
 import flask
-from flask_bootstrap import Bootstrap
-#import markdown
+
 import os
 import re
 
 
 
-def create_app():
-  app = flask.Flask(__name__)
-  Bootstrap(app)
 
-  return app
-
-app = create_app()
+app = flask.Flask(__name__)
 
 @app.route( '/' )
 def index():
@@ -38,17 +31,13 @@ def index():
 def view( proto, port ):
 	notes = ""
 
-	if( os.path.isdir( "data/{}/{}".format( proto, port ) ) ):
-		filename_notes = "data/{}/{}/notes".format( proto, port ) 
-		if( os.path.exists( filename_notes ) ):
-			notes = open( filename_notes, 'r' ).read().decode("utf-8").replace( "\n", "<br />" )
-		else:
-			notes = "{} doesn't exist".format( filename_notes )
-				
+	if( os.path.exists( filename_notes ) ):
+		notes = open( filename_notes, 'r' ).read().decode("utf-8").replace( "\n", "<br />" )
+	else:
+		notes = "{} doesn't exist".format( filename_notes )		
 	return flask.render_template( "view.html", proto=proto, port=port, notes=notes )
 
 
 if __name__ == '__main__':
 	app.run( debug=True )
 
-	#("0.0.0.0", 0 ), 
