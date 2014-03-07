56b6a66331829cf8d265d81783af333594178539
James Hodgkinson <yaleman@ricetek.net>
2014-03-07 20:18:10 +1000

    working on the osx infodump

--- scripts/infodump-osx.py
+++ scripts/infodump-osx.py
@@ -1,6 +1,17 @@
 #!/usr/bin/python
 
-import sys
+import subprocess
+import re
 
+command = [ "/usr/sbin/lsof", '+c', '0', '-n', '-i', '-P' ]
+# | /usr/bin/awk '{ print $1 \" \" $5  \" \" $8 \" \" $9 \" \" $10}'"
+re_lsof = re.compile( "^(?P<process>[^\s]+)[\s]+[\d]+[\s]+[^\s]+[\s]+[^\s]+[\s]+(?P<ipversion>[^\s]+)[\s]+[\S]+[\s]+[\S]+[\s]+(?P<protocol>[\S]+)[\s](?P<ipstring>[\S]+)[\s]*[\(]*(?P<connectiontype>[^\)]*)[\)]*" )
 
-command = "lsof +c 0 -n -i -P | awk '{ print $1 " " $5  " " $8 " " $9 " " $10}'"
+re_ipv4connect = re.compile( "")
+text = subprocess.check_output( command )
+for line in [ line for line in text.split( "\n" )[1:] if line.strip() != "" ]:
+	data = re_lsof.match( line )
+	if data != None:
+		if data.group( 'protocol' ) in [ 'TCP','UDP' ]:
+			if data.group('ipstring' ) != '*:*':
+				print data.group( 'protocol' ), data.group( 'ipstring' ), data.group( 'connectiontype' )
