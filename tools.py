df4a436d20bfbca177bc796997b7591647b7fba4
James Hodgkinson <yaleman@ricetek.net>
2014-07-17 12:03:56 +1000

    re-indented lev function

--- tools.py
+++ tools.py
@@ -38,22 +38,22 @@ def datadir( proto, port=None ):
     return  "data/{}/{}/".format( proto.lower(), port )
 
 def levenshtein(s1, s2):
-    if len(s1) < len(s2):
-        return levenshtein(s2, s1)
-
-    # len(s1) >= len(s2)
-    if len(s2) == 0:
-        return len(s1)
-
-    previous_row = xrange(len(s2) + 1)
-    for i, c1 in enumerate(s1):
-        current_row = [i + 1]
-        for j, c2 in enumerate(s2):
-            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
-            deletions = current_row[j] + 1       # than s2
-            substitutions = previous_row[j] + (c1 != c2)
-            current_row.append(min(insertions, deletions, substitutions))
-        previous_row = current_row
-
-    return previous_row[-1]
   """ calculate the levenshtein distance between two strings """
+  if len(s1) < len(s2):
+    return levenshtein(s2, s1)
+
+  # len(s1) >= len(s2)
+  if len(s2) == 0:
+    return len(s1)
+
+  previous_row = xrange(len(s2) + 1)
+  for i, c1 in enumerate(s1):
+    current_row = [i + 1]
+    for j, c2 in enumerate(s2):
+      insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+      deletions = current_row[j] + 1       # than s2
+      substitutions = previous_row[j] + (c1 != c2)
+      current_row.append(min(insertions, deletions, substitutions))
+    previous_row = current_row
+
+  return previous_row[-1]
