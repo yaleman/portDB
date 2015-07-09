806e3ff084d6b573799698192eec0f7892749212
yaleman <yaleman@ricetek.net>
2015-07-09 11:27:49 +1000

    submitting to pylints demands
    functions with no spaces
    fixed names of variables
    documented some things
    finished a TODO
    removed a default variable from avoidnasty

--- tools.py
+++ tools.py
@@ -1,46 +1,53 @@
 """ This is the tools library for the portDB project """
-import markdown
 
-import os
-from flask import Flask, render_template, request, url_for, jsonify, abort
+from os import path
+from flask import request, url_for, abort
+
 
 def url_for_other_page(page):
-    """ Documentation to come?
+    """ Provides the URL for another page  based on the function endpoint.
     """
-    #TODO: Document this function
     args = request.view_args.copy()
     args['page'] = page
     return url_for(request.endpoint, **args)
 
-def datadir( proto, port=None ):
-  """ String datadir( String proto, Int port )
-  returns the appropriate data directory based on the protocol/port supplied
-  """
-  # TODO: include checking for if the directory actually exists.
-  if "." in proto :
-    abort( 403 )
-  if port is None:
-    return 'data/{}/'.format( proto.lower() )
-  else:
-    return  "data/{}/{}/".format( proto.lower(), port )
+def datadir(proto, port=None):
+    """ String datadir( String proto, Int port )
+    returns the appropriate data directory based on the protocol/port supplied
+    """
+    if "." in proto:
+        abort(403)
+    if port is None:
+        protodir = 'data/{}/'.format(proto.lower())
+        # check if the path actually exists
+        if path.exists(protodir):
+            return protodir
+        else:
+            abort(404)
+    else:
+        linkdir = "data/{}/{}/".format(proto.lower(), port)
+        if path.exists(linkdir):
+            return linkdir
+        else:
+            abort(404)
 
-def levenshtein(s1, s2):
-  """ calculate the levenshtein distance between two strings """
-  if len(s1) < len(s2):
-    return levenshtein(s2, s1)
+def levenshtein(string1, string2):
+    """ calculate the levenshtein distance between two strings """
+    if len(string1) < len(string2):
+        return levenshtein(string2, string1)
 
-  # len(s1) >= len(s2)
-  if len(s2) == 0:
-    return len(s1)
+    # len(string1) >= len(string2)
+    if len(string2) == 0:
+        return len(string1)
 
-  previous_row = xrange(len(s2) + 1)
-  for i, c1 in enumerate(s1):
-    current_row = [i + 1]
-    for j, c2 in enumerate(s2):
-      insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
-      deletions = current_row[j] + 1       # than s2
-      substitutions = previous_row[j] + (c1 != c2)
-      current_row.append(min(insertions, deletions, substitutions))
+    previous_row = xrange(len(string2) + 1)
+    for i, character1 in enumerate(string1):
+        current_row = [i + 1]
+        for j, character2 in enumerate(string2):
+            insertions = previous_row[j + 1] + 1 # j+1 instead of j since previous_row and current_row are one character longer
+            deletions = current_row[j] + 1       # than string2
+            substitutions = previous_row[j] + (character1 != character2)
+            current_row.append(min(insertions, deletions, substitutions))
     previous_row = current_row
 
-  return previous_row[-1]
+    return previous_row[-1]
--- webinterface.py
+++ webinterface.py
@@ -1,15 +1,15 @@
 """ Web interface and implementation of portDB
 see the running version at http://portdb.yaleman.org """
-from flask import Flask, render_template, request, url_for, jsonify, abort
 
 
+from flask import Flask, render_template, abort
 import markdown
-import os.path
-from math import ceil
-from tools import *
+from os import path, listdir
+from tools import datadir, url_for_other_page
 import json
 
 
+PROTOCOLS = {'tcp' : "Transmission Control Protocol", 'udp' : "User Datagram Protocol"}
 
 
 # initialize the web app
@@ -17,154 +17,157 @@ PORTDB = Flask(__name__)
 PORTDB.jinja_env.globals['url_for_other_page'] = url_for_other_page
 
 # going to hard code the protocols
-#protocols = [ p.lower() for p in os.listdir( 'data/' ) if p != '.DS_Store' ]
-PROTOCOLS = { 'tcp' : "Transmission Control Protocol",
-      'udp' : "User Datagram Protocol"
-      }
+#protocols = [p.lower() for p in listdir('data/') if p != '.DS_Store']
 
 
-def portlist( proto=None ):
+
+def portlist(proto=None):
 	""" returns a list of ports (as strings) based on a proto """
 	# TODO: write tests for this, it should be doable.
-	if avoidnasty( proto ):
+	if avoidnasty(proto,  None):
 		proto = proto.lower()
-		ports = [ port for port in os.listdir( datadir( proto ) ) if port != '.DS_Store' ]
+		ports = [port for port in listdir(datadir(proto)) if port != '.DS_Store']
 		return ports
 
 
-def avoidnasty( proto, port=None ):
+def avoidnasty(proto, port):
 	"""
-  check against the stored protocols. fairly simple way of avoiding nastiness. terribly broken though.
-  """
-  #TODO: add valid-port checking
+	check against the stored protocols. fairly simple way of avoiding nastiness. terribly broken though.
+	"""
 	if proto.lower() not in PROTOCOLS:
-		abort( 403 )
-		return False
+		abort(404)
+        #return False
+	if port != None:
+		if str( port ) not in listdir('data/{}'.format(proto)):
+			abort(404)
 	return True
 
 
-@PORTDB.route( '/' )
+@PORTDB.route('/')
 def index():
 	""" the site homepage, lists protocols """
-	return render_template( "index.html", protocols=PROTOCOLS )
+	return render_template("index.html", protocols=PROTOCOLS)
 
 
-@PORTDB.route( '/about' )
+@PORTDB.route('/about')
 def about():
   """ return the README.md file in the default template """
-  return render_template( "about.html", readme=markdown.markdown( open( 'README.md', 'r' ).read() ) )
+  return render_template("about.html", readme=markdown.markdown(open('README.md', 'r').read()))
 
 
-@PORTDB.route( '/contributing' )
+@PORTDB.route('/contributing')
 def contributing():
   """ displays the page where people can find out about helping """
-  return render_template( "contributing.html" )
+  return render_template("contributing.html")
 
 
 @PORTDB.route('/view/<proto>', defaults={'page': 1})
 @PORTDB.route('/view/<proto>/page/<int:page>')
-def viewproto( proto, page ):
+def viewproto(proto, page):
 	""" view a list of ports associated with this protocol """
-	if avoidnasty( proto ):
+	if avoidnasty(proto, None):
 		from pagination import Pagination
 
-		if os.path.exists( datadir( proto ) ):
-			ports = [ port for port in os.listdir( datadir( proto ) ) if port != '.DS_Store' ]
+		if path.exists(datadir(proto)):
+			ports = [port for port in listdir(datadir(proto)) if port != '.DS_Store']
 			# count the ports for pagination's sake
-			num_ports = len( ports )
+			num_ports = len(ports)
 			per_page = 500
 
 			if not ports and page != 1:
 			# if the list of ports is broken and someone's going to the wrong page, 404 them
-				abort( 404 )
+				abort(404)
 			else:
 				# sort them numerically instead of alphabetically.
-				ports.sort( key=int )
+				ports.sort(key=int)
 				if page == 1:
 					startpoint = 0
 				else:
-					startpoint = ( page -1 ) * per_page
-				ports = ports[ startpoint : ( startpoint + per_page ) ]
-				numports = len( ports )
+					startpoint = (page -1) * per_page
+				ports = ports[startpoint : (startpoint + per_page)]
+				numports = len(ports)
 
-			return render_template( "viewproto.html", pagination=Pagination( page, per_page, num_ports ),
-					proto=proto, ports=ports, numports=numports, startpoint=startpoint )
+			return render_template("viewproto.html", pagination=Pagination(page, per_page, num_ports), proto=proto, ports=ports, numports=numports, startpoint=startpoint)
 		else:
 			return index()
 
 
-@PORTDB.route('/view/<proto>/<int:port>', methods=['GET'] )
-@PORTDB.route('/view/<proto>/<int:port>/', methods=['GET'] )
-def view( proto, port ):
+@PORTDB.route('/view/<proto>/<int:port>', methods=['GET'])
+@PORTDB.route('/view/<proto>/<int:port>/', methods=['GET'])
+def view(proto, port):
   """" presents the view of a protocol/port combination """
   proto = proto.lower()
-  if avoidnasty( proto, port ):
-		ianafile = '{}iana.md'.format( datadir( proto, port ) )
-		notesfile = '{}notes.md'.format( datadir( proto, port ) )
+  if avoidnasty(proto, port):
+		ianafile = '{}iana.md'.format(datadir(proto, port))
+		notesfile = '{}notes.md'.format(datadir(proto, port))
 		iana = False
 		notes = False
-		if os.path.exists( ianafile ):
-			iana = markdown.markdown( open( ianafile, 'r' ).read() )
-		if os.path.exists( notesfile ):
-			with open( notesfile, 'r' ) as fh:
-				notes = markdown.markdown( fh.read().decode( 'utf-8' ) )
-		return render_template( "view.html", proto=proto, port=port, notes=notes, iana=iana )
+		if path.exists(ianafile):
+			iana = markdown.markdown(open(ianafile, 'r').read())
+		if path.exists(notesfile):
+			with open(notesfile, 'r') as filehandle:
+				notes = markdown.markdown(filehandle.read().decode('utf-8'))
+		return render_template("view.html", proto=proto, port=port, notes=notes, iana=iana)
 
-def searchports( proto, searchterm ):
+def searchports(proto, searchterm):
 	""" supply the proto and the searchterm and it'll respond with the list of ports and if there's more than 5 """
 	# TODO : write tests for this, it should be doable.
 	maxresponses = 10
 
-	ports = [ port for port in portlist( proto ) if searchterm in port ]
-	ports.sort( key=int )
+	ports = [port for port in portlist(proto) if searchterm in port]
+	ports.sort(key=int)
 	ismore = False
-	if len( ports ) > maxresponses:
+	if len(ports) > maxresponses:
 		ismore = True
 		ports = ports[:maxresponses]
 	return ports, ismore
 
 
-@PORTDB.route( '/api/search/<searchterm>/search.json', methods=[ 'GET' ] )
-def apisearch( searchterm ):
+@PORTDB.route('/api/search/<searchterm>/search.json', methods=['GET'])
+def apisearch(searchterm):
   """ this is the start of the API search functionality, still doesn't work """
   data = []
   for proto in PROTOCOLS:
-    tmp = [ port for port in portlist( proto ) if searchterm in port ]
+    tmp = [port for port in portlist(proto) if searchterm in port]
     for port in tmp:
-      data.append( { 'proto' : proto, 'port' : port } )
-  return json.dumps( data )
+      data.append({'proto' : proto, 'port' : port})
+  return json.dumps(data)
 
-@PORTDB.route( '/api/<proto>/<port>/search.json', methods=[ 'GET' ] )
-@PORTDB.route( '/api/<proto>/proto.json', defaults={ 'port': "" }, methods=[ 'GET' ] )
-def api( proto, port ):
+@PORTDB.route('/api/<proto>/<port>/search.json', methods=['GET'])
+@PORTDB.route('/api/<proto>/proto.json', defaults={'port': ""}, methods=['GET'])
+def api(proto, port):
   """ returns a json object which should allow searches for things."""
-  ports, ismore = searchports( proto, port )
+  ports, ismore = searchports(proto, port)
   data = {}
-  data['ports'] = list( [ ( proto, port ) for port in ports ] )
+  data['ports'] = list([(proto, port) for port in ports])
   data['ismore'] = ismore
-  return json.dumps( data )
+  return json.dumps(data)
 
 
-@PORTDB.route('/robots.txt' )
-def robots( ):
+@PORTDB.route('/robots.txt')
+def robots():
     """ return the robots.txt file """
     return 'User-agent: *\nDisallow:'
 
-    
+
 @PORTDB.errorhandler(404)
-def error404( error ):
+def error404(error):
   """ returns a 404 whenever needed """
-  return render_template( 'errors/404.html' ), 404
+  if error:
+      pass
+  return render_template('errors/404.html'), 404
 
 
 @PORTDB.errorhandler(500)
-def error500( error ):
+def error500(error):
   """ returns a 500 error whenever needed """
-  return render_template( 'errors/500.html' ), 500
+  if error:
+      pass
+  return render_template('errors/500.html'), 500
 
 
 @PORTDB.after_request
-def add_header( response ):
+def add_header(response):
     """
     Add headers to both force latest IE rendering engine or Chrome Frame,
     and also to cache the rendered page for 10 minutes.
@@ -175,4 +178,4 @@ def add_header( response ):
 
 
 if __name__ == '__main__':
-	PORTDB.run( debug=True )
+	PORTDB.run(debug=True)
